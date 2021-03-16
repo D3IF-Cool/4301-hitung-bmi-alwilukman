a24f978eb4bf8ac0e80a50f1c840f1df3570f04b
M_Alwi_Lukman_N <alwilukman17@gmail.com>
2021-03-16 21:27:09 +0700

    3.1.Refactor fitur hitung BMI ke fragment

---
+++ app/src/main/java/org/d3if0074/hitungbmi/data/KategoriBmi.kt
@@ -0,0 +1,4 @@
+package org.d3if0074.hitungbmi.data
+
+enum class KategoriBmi { KURUS, IDEAL, GEMUK
+}
---
+++ app/src/main/java/org/d3if0074/hitungbmi/ui/SaranFragment.kt
@@ -0,0 +1,45 @@
+package org.d3if0074.hitungbmi.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import org.d3if0074.hitungbmi.R
+import org.d3if0074.hitungbmi.data.KategoriBmi
+import org.d3if0074.hitungbmi.databinding.FragmentSaranBinding
+
+class SaranFragment : Fragment() {
+    private lateinit var binding: FragmentSaranBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentSaranBinding.inflate(
+                layoutInflater, container, false)
+        updateUI(KategoriBmi.KURUS)
+        return binding.root
+    }
+    private fun updateUI(kategori: KategoriBmi) {
+        val actionBar = (requireActivity() as AppCompatActivity)
+                .supportActionBar
+        when (kategori) {
+            KategoriBmi.KURUS -> {
+                actionBar?.title = getString(R.string.judul_kurus)
+                binding.imageView.setImageResource(R.drawable.ic_launcher_foreground)
+                binding.textView.text = getString(R.string.saran_kurus)
+            }
+            KategoriBmi.IDEAL -> {
+                actionBar?.title = getString(R.string.judul_ideal)
+                binding.imageView.setImageResource(R.drawable.ic_launcher_foreground)
+                binding.textView.text = getString(R.string.saran_ideal)
+            }
+            KategoriBmi.GEMUK -> {
+                actionBar?.title = getString(R.string.judul_gemuk)
+                binding.imageView.setImageResource(R.drawable.ic_launcher_foreground)
+                binding.textView.text = getString(R.string.saran_gemuk)
+            }
+        }
+    }
+
+
+}
